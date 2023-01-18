197dcb938540608a634c40cffed935c7bf29f256
Sami Abdul-Wahid <syntapy@pm.me>
2023-01-18 14:12:02 -0800

    fails with cy.viewport call

--- cypress/e2e/viewport.cy.js
+++ cypress/e2e/viewport.cy.js
@@ -6,9 +6,9 @@ describe('Sample Tests', () => {
     cy.wrap(vpWidth).should('not.be.undefined')
     cy.wrap(vpHeight).should('not.be.undefined')
 
-    //cy.viewport(vpWidth+100, vpHeight+100)
-    Cypress.config('viewportWidth', vpWidth+100)
-    Cypress.config('viewportHeight', vpHeight+100)
+    cy.viewport(vpWidth+100, vpHeight+100)
+    //Cypress.config('viewportWidth', vpWidth+100)
+    //Cypress.config('viewportHeight', vpHeight+100)
 
     const vpWidthNew = Cypress.config('viewportWidth')
     const vpHeightNew = Cypress.config('viewportHeight')
@@ -16,9 +16,9 @@ describe('Sample Tests', () => {
     cy.wrap(vpWidthNew).should('equal', vpWidth+100)
     cy.wrap(vpHeightNew).should('equal', vpHeight+100)
 
-    //cy.viewport(vpWidth, vpHeight)
-    Cypress.config('viewportWidth', vpWidth)
-    Cypress.config('viewportHeight', vpHeight)
+    cy.viewport(vpWidth, vpHeight)
+    //Cypress.config('viewportWidth', vpWidth)
+    //Cypress.config('viewportHeight', vpHeight)
 
     const vpWidthOriginal = Cypress.config('viewportWidth')
     const vpHeightOriginal = Cypress.config('viewportHeight')
