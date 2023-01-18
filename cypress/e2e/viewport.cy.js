ccef4254e975c4e88480e0f2db93b41ca506fc20
Sami Abdul-Wahid <syntapy@pm.me>
2023-01-18 14:29:24 -0800

    demonstrate

--- cypress/e2e/viewport.cy.js
+++ cypress/e2e/viewport.cy.js
@@ -1,3 +1,8 @@
+function setViewport(width, height) {
+  Cypress.config('viewportWidth', width)
+  Cypress.config('viewportHeight', height)
+}
+
 describe('Sample Tests', () => {
   it('viewport change', () => {
     cy.visit('/')
@@ -6,9 +11,9 @@ describe('Sample Tests', () => {
     cy.wrap(vpWidth).should('not.be.undefined')
     cy.wrap(vpHeight).should('not.be.undefined')
 
-    cy.setViewport(vpWidth+100, vpHeight+100)
-    //Cypress.config('viewportWidth', vpWidth+100)
-    //Cypress.config('viewportHeight', vpHeight+100)
+    cy.setViewport(vpWidth+100, vpHeight+100)   // custom command: does not work
+    cy.viewport(vpWidth+100, vpHeight+100)      // does not work
+    setViewport(vpWidth+100, vpHeight+100)      // works
 
     const vpWidthNew = Cypress.config('viewportWidth')
     const vpHeightNew = Cypress.config('viewportHeight')
@@ -16,9 +21,9 @@ describe('Sample Tests', () => {
     cy.wrap(vpWidthNew).should('equal', vpWidth+100)
     cy.wrap(vpHeightNew).should('equal', vpHeight+100)
 
-    cy.setViewport(vpWidth, vpHeight)
-    //Cypress.config('viewportWidth', vpWidth)
-    //Cypress.config('viewportHeight', vpHeight)
+    cy.setViewport(vpWidth, vpHeight)       // custom command: does not work
+    cy.viewport(vpWidth, vpHeight)          // does not work
+    setViewport(vpWidth, vpHeight)          // works
 
     const vpWidthOriginal = Cypress.config('viewportWidth')
     const vpHeightOriginal = Cypress.config('viewportHeight')
